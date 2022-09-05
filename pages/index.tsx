b424fa0456d74d594711806f42915aefb303f908
Max Antony <maxpacami@gmail.com>
2022-09-05 11:44:27 -0500

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,10 +20,11 @@ const Home: NextPage = () => {
     <div className={`absolute top-0 bottom-0 flex pl-5 lg:pl-24`}>
       <div className='flex items-center'>
         <div className='group flex flex-col'>
+          <p>Hola, me llamo</p>
           <h3 className='mb-6 text-4xl font-bold xl:text-7xl'>Max Antony</h3>
           <span className='mb-6 h-1 w-10 bg-gray-700 transition-[width] duration-500 group-hover:w-full lg:w-16'></span>
           <p className='mb-7 text-xl text-gray-700'>
-            Desarrollador <span className='font-bold'>Web</span>
+            Soy Desarrollador <span className='font-bold'>Web</span>
           </p>
           <p className='mb-7 text-xl text-gray-700'>
             y hago{' '}
