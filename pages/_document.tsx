6494461e473c1cba81f296db9c38e00251c7fcff
Max Antony <maxpacami@gmail.com>
2022-09-08 14:54:43 -0500

    update: order Head in app and document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,27 +11,6 @@ export default class CustomDocument extends Document {
     return (
       <Html lang='es-PE'>
         <Head>
-          <meta name="application-name" content="Max Antony" />
-          <meta name="description" content="Pagina personal de Max Antony" />
-          <meta name='author' content='Max Antony Paucar Ramirez' />
-          <meta name='keywords' content='Max Antony, Paucar Ramirez, Huancayo, Junin, Peru, Programacion, Desarrollador, Sistemas, Paginas web, Marketing' />
-          <meta name="format-detection" content="telephone=no" />
-          <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="theme-color" content="#000000" />
-          <link rel="icon" type="image/jpg" sizes="192x192" href="/icons/manifest-icon-192.jpg" />
-          <link rel="icon" type="image/jpg" sizes="512x512" href="/icons/manifest-icon-512.jpg" />
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="shortcut icon" href="/favicon.ico" />
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content="Max Antony" />
-          <meta property="og:description" content="Pagina personal de Max Antony" />
-          <meta property="og:site_name" content="Max Antony" />
-          <meta property="og:url" content="https://maxantony.com" />
-          <meta property="og:image" content="https://maxantony.com/icons/manifest-icon-512.jpg" />
-          <meta
-            name='viewport'
-            content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
-          />
           {/*Google fonts: Poppins*/}
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap" rel="stylesheet" />
         </Head>
